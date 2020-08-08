3664180fdc142a3b5fc6c39c58bef0b0079fc94c
Thomaz <torresthomaz648@gmail.com>
2020-08-08 17:31:01 -0300

    refactored

--- src/server.js
+++ src/server.js
@@ -1,30 +1,7 @@
 //Servidor
 const express = require('express')
 const server = express()
-
-function pageLanding(req, res) {
-    return res.render("index.html")
-}
-
-function pageStudy(req, res) {
-    const  filters = req.query
-    return res.render("study.html", {proffys, filters, subjects, weekdays})
-}
-
-function pageGiveClasses(req, res) {
-    const data = req.query
-
-    const isNotEmpty = Object.keys(data).length > 0
-    //se tiver dados
-    if(isNotEmpty != "") {
-        data.subject = getSubject(data.subject)
-        //adicionar dados a lista de proffys
-        proffys.push(data)
-        return res.redirect("/study")
-    }
-    //se não, não adicionar
-    return res.render("give-classes.html", {subjects, weekdays})
-}
+const { pageLanding, pageStudy, pageGiveClasses} = require('./pages')
 
 //configurar nunjucks(template engine)
 const nunjucks = require('nunjucks')

65d7613793699b865beba374200e8d0f9dfe34ad
Thomaz <torresthomaz648@gmail.com>
2020-08-09 15:28:41 -0300

    pages ready

--- src/pages.js
+++ src/pages.js
@@ -36,6 +36,10 @@ async function pageStudy(req, res) {
         const db = await Database
         const proffys = await db.all(query)
 
+        proffys.map((proffy) => {
+            proffy.subject = getSubject(proffy.subject)
+        })
+
         return res.render("study.html", {proffys, subjects, filters, weekdays})
     } catch (error) {
         console.log(error)
@@ -43,22 +47,51 @@ async function pageStudy(req, res) {
 }
 
 function pageGiveClasses(req, res) {
-    const data = req.query
-
-    const isNotEmpty = Object.keys(data).length > 0
-    //se tiver dados
-    if(isNotEmpty) {
-        data.subject = getSubject(data.subject)
-        //adicionar dados a lista de proffys
-        proffys.push(data)
-        return res.redirect("/study")
-    }
-    //se não, não adicionar
     return res.render("give-classes.html", {subjects, weekdays})
+}
+
+async function saveClasses(req, res) {
+    const crateProffy = require('./database/createProffy')
+    
+    const proffyValue = {
+        name: req.body.name,
+        avatar: req.body.avatar,
+        whatsap: req.body.whatsapp,
+        bio: req.body.bio
+    }
+
+    const classValue = {
+        subject: req.body.subject,
+        cost: req.body.cost
+    }
+
+    const classScheduleValues = req.body.weekday.map(
+        (weekday, index) => {
+            return {
+                weekday,
+                time_from: convertHoursToMinutes(req.body.time_from[index]),
+                time_to: convertHoursToMinutes(req.body.time_to[index])
+            }
+    })
+
+    try {
+        const db = await Database
+        await crateProffy(db, {proffyValue, classValue, classScheduleValues})
+
+        let queryString = "?subject=" + req.body.subject
+        queryString += "&weekday=" + req.body.weekday[0]
+        queryString += "&time=" + req.body.time_from[0]
+
+        return res.redirect("/study" + queryString)
+    } catch (error) {
+        console.log(error)
+    }
+    
 }
 
 module.exports = {
     pageLanding,
     pageStudy,
-    pageGiveClasses
+    pageGiveClasses,
+    saveClasses
 }
